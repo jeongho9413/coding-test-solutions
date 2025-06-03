cacfe71d034139bc447364ffb1e7d9389a6ff560
Jeongho Ahn <ahn@irvs.ait.kyushu-u.ac.jp>
2025-06-04 06:19:27 +0900

    Create jeongho_ver2.py

---
+++ atcoder/boot-camp-for-begineers/b-power-socket/jeongho_ver2.py
@@ -0,0 +1,15 @@
+import sys
+import os
+import math
+
+def solution(a: int, b: int) -> int:
+    if b == 1: return 0
+    elif a >= b: return 1
+    else: return math.ceil((b - 1)/(a - 1))
+
+"""
+inputs: 4, 10
+outputs: 3
+"""    
+a, b = list(map(int, input().split()))
+print(solution(a, b))

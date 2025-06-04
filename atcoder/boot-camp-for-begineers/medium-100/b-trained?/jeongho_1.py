70fb60a33857eea4c6c3c931e51a346eee53d3ef
Jeongho Ahn <ahn@irvs.ait.kyushu-u.ac.jp>
2025-06-05 07:48:01 +0900

    Create jeongho_1.py

---
+++ atcoder/boot-camp-for-begineers/medium-100/b-trained?/jeongho_1.py
@@ -0,0 +1,27 @@
+"""
+https://atcoder.jp/contests/abc065/tasks/abc065_b
+"""
+import sys
+
+def main() -> None:
+    input = sys.stdin.readline
+    n = int(input())
+    nxt = [0] + [int(input()) for _ in range(n)]
+    
+    cur = 1
+    step = 0
+    visited = [False] * (n + 1)
+    
+    while True:
+        if cur == 2:
+            print(step)
+            return
+        if visited[cur]:
+            print(-1)
+            return
+        visited[cur] = True
+        cur = nxt[cur]
+        step += 1
+        
+if __name__ == "__main__":
+    main()

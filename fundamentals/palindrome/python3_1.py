ac9cba4d3189a112e600381263818a684a01e7b4
Jeongho Ahn <ahn@irvs.ait.kyushu-u.ac.jp>
2025-06-18 21:39:28 +0900

    Create python3_1.py

---
+++ fundamentals/palindrome/python3_1.py
@@ -0,0 +1,12 @@
+import sys
+
+def is_palindrome(s: str) -> bool:
+  a = [c.lower() for c in s if c.isalnum()]
+  return a == a[::-1]
+
+def main() -> None:
+  a = str(input())
+  print(is_palindrome(a))
+
+if __name__ == "__main__":
+  main()

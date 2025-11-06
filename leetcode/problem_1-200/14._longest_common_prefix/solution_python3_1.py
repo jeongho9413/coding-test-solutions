66039433bf3c2c93d01d1229a2f9de85af9d08e7
Jeongho Ahn <ahn@irvs.ait.kyushu-u.ac.jp>
2025-11-06 15:12:47 +0900

    Create solution_python3_1.py

---
+++ leetcode/problem_1-200/14._longest_common_prefix/solution_python3_1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+
+        #
+        min_len = float("inf")
+        for s in strs:
+            if len(s) < min_len:
+                min_len = len(s)
+        
+        #
+        i = 0
+        while i < min_len:
+            for s in strs[1:]:
+                if s[i] != strs[0][i]:
+                    return s[:i]
+            i += 1
+
+        #
+        return s[:i]

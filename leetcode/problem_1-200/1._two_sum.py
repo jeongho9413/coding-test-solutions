8e97da3a854d63b83b78f945999667489bbc4235
Jeongho Ahn <ahn@irvs.ait.kyushu-u.ac.jp>
2025-11-06 13:02:06 +0900

    Update 1._two_sum.py

--- leetcode/problem_1-200/1._two_sum.py
+++ leetcode/problem_1-200/1._two_sum.py
@@ -1,12 +1,12 @@
 class Solution:
-  def twoSum(self, nums: List[int], target: int) -> List[int]:
-    
-    #
-    hm = dict()
-    
-    #
-    for i, num in enumerate(nums):
-      if target - num in hm:
-        return [i, hm[target - num]]
-      else:
-        hm[num] = i
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        #
+        hm = dict()
+
+        #
+        for i, num in enumerate(nums):
+            if target - num in hm:
+                return [i, hm[target - num]]
+            else:
+                hm[num] = i

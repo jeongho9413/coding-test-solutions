100a34c9233160bcf22d84ba42c766b18c2c6791
Jeongho Ahn <ahn@irvs.ait.kyushu-u.ac.jp>
2025-11-06 13:35:48 +0900

    Create solution_python3_1.py

---
+++ leetcode/problem_1-200/9._palindrome_number/solution_python3_1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+
+        #
+        if x < 0:
+            return False
+
+        #
+        rev, orig = 0, x
+
+        while x > 0:
+            rev = rev * 10 + x % 10
+            x //= 10
+
+        return rev == orig

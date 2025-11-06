cf7c2cb8f64bde2820a08c59188ef13f47b1f729
Jeongho Ahn <ahn@irvs.ait.kyushu-u.ac.jp>
2025-11-07 03:21:46 +0900

    Create solution_python3_1.py

---
+++ leetcode/problem_1-200/39._combination_sum/solution_python3_1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+
+        #
+        res = list()
+
+        #
+        def dfs(i, comb, total):
+            if i >= len(candidates) or total > target:
+                return
+
+            if total == target:
+                res.append(comb.copy())
+                return
+
+            comb.append(candidates[i])
+            dfs(i, comb, total + candidates[i])
+            comb.pop()
+            dfs(i + 1, comb, total)
+
+        #
+        dfs(0, [], 0)
+        return res

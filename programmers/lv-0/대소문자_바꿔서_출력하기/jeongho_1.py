99664d4ea9d4533a1410db4e3242a1b9adad0446
Jeongho Ahn <ahn@irvs.ait.kyushu-u.ac.jp>
2025-11-07 04:18:07 +0900

    Delete programmers/lv-0 directory

--- programmers/lv-0/대소문자_바꿔서_출력하기/jeongho_1.py
+++
@@ -1,6 +0,0 @@
-"""
-https://school.programmers.co.kr/learn/courses/30/lessons/181949
-"""
-
-str_1 = str(input())
-print(str_1.swapcase())
--- programmers/lv-0/특수문자_출력하기/jeongho_1.py
+++
@@ -1,6 +0,0 @@
-"""
-https://school.programmers.co.kr/learn/courses/30/lessons/181948
-"""
-
-result = "!@#$%^&*(\\\'\"<>?:;"
-print(result)

73937841bbd59c2b5ab10f7b967c12e9273d8b24
Jeongho Ahn <ahn@irvs.ait.kyushu-u.ac.jp>
2025-11-11 15:52:06 +0900

    Create solution_python3_1.py

---
+++ leetcode/problem_1-200/75._sort-colors/solution_python3_1.py
@@ -0,0 +1,33 @@
+"""
+leetcode - 75. sort colors
+using pointers (Dutch National Flag Algorithm)
+time: O(n)
+space: O(1)
+"""
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        #
+        def swap(i, j):
+            temp = nums[i]
+            nums[i] = nums[j]
+            nums[j] = temp
+        
+        #
+        n = len(nums)
+        l, r = 0, n - 1
+        i = 0
+
+        #
+        while i <= r:
+            if nums[i] == 0:
+                swap(l, i)
+                l += 1
+            elif nums[i] == 2:
+                swap(i, r)
+                r -= 1
+                i -= 1
+            i += 1

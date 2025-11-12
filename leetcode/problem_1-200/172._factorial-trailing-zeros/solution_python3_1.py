40bf6062efeb343136274a9a7e0fca5ac4605872
Jeongho Ahn <ahn@irvs.ait.kyushu-u.ac.jp>
2025-11-13 08:44:36 +0900

    Create solution_python3_1.py

---
+++ leetcode/problem_1-200/172._factorial-trailing-zeros/solution_python3_1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+
+        #
+        res = 0
+
+        #
+        while n > 0:
+            n //= 5
+            res += n
+
+        return res

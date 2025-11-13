c75c05c1ec86b1f625286068f1ded580813bdafb
Jeongho Ahn <ahn@irvs.ait.kyushu-u.ac.jp>
2025-11-13 19:47:53 +0900

    Update and rename solution_python3_1.py to solution_python3_1.py

--- leetcode/problem_1-200/5._longest-palindromic-substring/solution_python3_1.py
+++ leetcode/problem_1-200/5._longest-palindromic-substring/solution_python3_1.py
@@ -1,3 +1,9 @@
+"""
+leetcode - 5. longest palindromic substring
+approach: using a dfs
+time: O(n^2)
+space: O(n^2)
+"""
 class Solution:
     def longestPalindrome(self, s: str) -> str:
 
@@ -8,13 +14,12 @@ class Solution:
         #
         def dfs(l, r):
             if l >= r:
-                return 1
-
+                return True
             if cache[l][r] is not None:
                 return cache[l][r]
             
             if s[l] != s[r]:
-                return 0
+                return False
             else:
                 cache[l][r] = dfs(l + 1, r - 1)
                 return cache[l][r]
@@ -26,9 +31,8 @@ class Solution:
         for i in range(n):
             for j in range(i + 1, n):
                 if j - i + 1 <= best_len:
-                    continue
-                
-                if dfs(i, j) == 1:
+                    continue           
+                if dfs(i, j) == True:
                     best_len = j - i + 1
                     best_l = i
 

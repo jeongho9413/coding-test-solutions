f8d8dc1af0bc73a6b68d86894f71a5056f547a8e
Jeongho Ahn <totoroahn@gmail.com>
2026-05-12 18:50:50 +0900

    Update solution_1.py

--- programmers/level-1/폰켓몬/python3/solution_1.py
+++ programmers/level-1/폰켓몬/python3/solution_1.py
@@ -1,7 +1,9 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/1845
-# time complexity: O(N)
-# space complexity: O(N)
+# strategy: hashset
+# time: O(N)
+# space: O(N)
 def solution(nums):
+    
     max_pick = len(nums) // 2
     unique_types = len(set(nums))  # time: O(N)
     

b25af2f6fcaa3533531be5a42a2e5192e05a53e6
Jeongho Ahn <totoroahn@gmail.com>
2026-05-12 14:11:18 +0900

    Create solution_1.py

---
+++ programmers/level-1/최소직사각형/python3/solution_1.py
@@ -0,0 +1,16 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/86491
+# strategy: brute-force
+# time complexity: O(N)
+# space complexity: O(1)
+def solution(sizes):
+    
+    max_w, max_h = 0, 0
+    
+    for w, h in sizes:
+        longer = max(w, h)
+        shorter = min(w, h)
+        
+        max_w = max(max_w, longer)
+        max_h = max(max_h, shorter)
+        
+    return max_w * max_h

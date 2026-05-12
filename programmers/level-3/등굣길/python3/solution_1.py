c299d454f8f9afd0566cae7be714f3f2f7f1c615
Jeongho Ahn <totoroahn@gmail.com>
2026-05-13 08:55:46 +0900

    Create solution_1.py

---
+++ programmers/level-3/등굣길/python3/solution_1.py
@@ -0,0 +1,21 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/42898
+# Strategy: Bottom-up DP
+# Time: O(n \times m)
+# Space: O(n \times m)
+def solution(m, n, puddles):
+    
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    puddle_set = set([(y, x) for x, y in puddles])
+    dp[1][1] = 1
+    
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if i == 1 and j == 1:
+                continue
+                
+            if (i, j) in puddle_set:
+                dp[i][j] = 0
+            else:
+                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000007
+                
+    return dp[n][m]

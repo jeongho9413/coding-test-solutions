8a762f3d022c0ffa2012b1fb2a065fae16559820
Jeongho Ahn <totoroahn@gmail.com>
2026-05-12 18:44:16 +0900

    Create solution_1.py

---
+++ programmers/level-1/체육복/python3/solution_1.py
@@ -0,0 +1,27 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/42862
+# strategy: greedy with set difference
+# time: O(N log N)
+# space: O(N)
+def solution(n, lost, reserve):
+    
+    _reserve = [r for r in reserve if r not in lost]
+    _lost = [l for l in lost if l not in reserve]
+    """
+    _reserve = set(reserve) - set(lost)
+    _lost = set(lost) - set(reserve)
+    """
+    
+    _reserve.sort()
+    _lost.sort()
+    
+    # for r in sorted(_reserve)
+    for r in _reserve:
+        f = r - 1
+        b = r + 1
+        
+        if f in _lost:
+            _lost.remove(f)
+        elif b in _lost:
+            _lost.remove(b)
+    
+    return n - len(_lost)

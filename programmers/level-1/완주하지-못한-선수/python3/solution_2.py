8ed3e2996525fbb466bc8d37296062881d48d874
Jeongho Ahn <totoroahn@gmail.com>
2026-05-12 14:26:21 +0900

    Update solution_2.py

--- programmers/level-1/완주하지-못한-선수/python3/solution_2.py
+++ programmers/level-1/완주하지-못한-선수/python3/solution_2.py
@@ -1,11 +1,13 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42576
+# strategy: hashmap
+# time complexity: 
+# space complexity: 
 def solution(participant, completion):
     
     from collections import Counter
     
     p_counter = Counter(participant)
     c_counter = Counter(completion)
-    answer = list((p_counter - c_counter).keys())[0]
-    return answer
-    # answer = ''
-    # return answer
+    res = list((p_counter - c_counter).keys())[0]
+    
+    return res

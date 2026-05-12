1879228a142b4d80052719857d60731df3f7963c
Jeongho Ahn <totoroahn@gmail.com>
2026-05-12 15:53:14 +0900

    Create solution_1.py

---
+++ programmers/level-2/H-Index/python3/solution_1.py
@@ -0,0 +1,13 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/42747
+# strategy: sorting 
+# time: O(N log N)
+# space: O(N)
+def solution(citations):
+    
+    citations.sort(reverse=True)
+    
+    for i, c in enumerate(citations):
+        if c < i + 1:
+            return i
+        
+    return len(citations)

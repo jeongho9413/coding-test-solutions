bc4e309805fbfefca98a21fb7e644d42e5315d41
Jeongho Ahn <totoroahn@gmail.com>
2026-05-13 07:19:33 +0900

    Create solution_1.py

---
+++ programmers/level-3/여행경로/python3/solution_1.py
@@ -0,0 +1,28 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/43164
+# strategy: DFS with eulerian path
+# time: O(N log N)
+# space: O(N)
+from collections import defaultdict
+
+def solution(tickets):
+    
+    graph = defaultdict(list)
+    
+    for start, end in tickets:
+        graph[start].append(end)
+        
+    for key in graph:
+        graph[key].sort(reverse=True)
+        
+    stack = ["ICN"]
+    path = []
+    
+    while stack:
+        top = stack[-1]
+        
+        if top in graph and graph[top]:
+            stack.append(graph[top].pop())  # O(1)
+        else:
+            path.append(stack.pop())
+            
+    return path[::-1]

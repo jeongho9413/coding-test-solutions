ce06df1b1cf3ef403e93a0777a745b26aa0be85f
Jeongho Ahn <totoroahn@gmail.com>
2026-05-14 05:09:31 +0900

    Update solution_1.py

--- programmers/level-3/입국심사/python3/solution_1.py
+++ programmers/level-3/입국심사/python3/solution_1.py
@@ -2,6 +2,12 @@
 # Strategy: Parametric Search / Binary Search
 # Time: O(L \times log R)
 # Space: O(1)
+# Similar problems: 
+# 입국심사 | https://school.programmers.co.kr/learn/courses/30/lessons/43238
+# 징검다리 | https://school.programmers.co.kr/learn/courses/30/lessons/43236
+# 예산 | https://school.programmers.co.kr/learn/courses/30/lessons/12982
+# 징검다리 건너기 | https://school.programmers.co.kr/learn/courses/30/lessons/64062
+# 금과 은 운반하기 | https://school.programmers.co.kr/learn/courses/30/lessons/86053
 def solution(n, times):
     
     l, r = 1, max(times) * n

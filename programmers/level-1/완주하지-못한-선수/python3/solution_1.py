d12ad280881ae29e50ac3544b1a8c95379a4b245
Jeongho Ahn <totoroahn@gmail.com>
2026-05-14 05:57:36 +0900

    Update solution_1.py

--- programmers/level-1/완주하지-못한-선수/python3/solution_1.py
+++ programmers/level-1/완주하지-못한-선수/python3/solution_1.py
@@ -1,16 +1,17 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42576
+# Strategy: Hashmap
+# Time: O(n)
+# Space: O(n)
 def solution(participant, completion):
     
     hm = dict()
     
-    for p in participant:  # O(N)
+    for p in participant:  # O(n)
         hm[p] = hm.get(p, 0) + 1
         
-    for c in completion:  # O(M)
+    for c in completion:  # O(m)
         hm[c] -= 1
         
-    for key, val in hm.items():  # O(N)
+    for key, val in hm.items():  # O(n)
         if val > 0:
             return key
-    # answer = ''
-    # return answer

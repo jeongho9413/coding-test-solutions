81cef20ff4170f01f3e216a0b17c3fd6beacd3bb
Jeongho Ahn <totoroahn@gmail.com>
2026-05-14 06:01:36 +0900

    Update solution_2.py

--- programmers/level-1/완주하지-못한-선수/python3/solution_2.py
+++ programmers/level-1/완주하지-못한-선수/python3/solution_2.py
@@ -1,13 +1,14 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42576
-# strategy: hashmap
-# time complexity: 
-# space complexity: 
+# Strategy: Hashmap using Counter
+# Time: O(n) 
+# Space: O(n)
+from collections import Counter
+
 def solution(participant, completion):
-    
-    from collections import Counter
     
     p_counter = Counter(participant)
     c_counter = Counter(completion)
+    
     res = list((p_counter - c_counter).keys())[0]
     
     return res

f61b2298ee0806570ced992e89c94c9937cffc94
Jeongho Ahn <totoroahn@gmail.com>
2026-05-17 12:04:28 +0900

    Create solution_1.py

---
+++ programmers/level-2/124-나라의-숫자/python3/solution_1.py
@@ -0,0 +1,16 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12899
+# Strategy: Ternary numeral system
+# Time: O(log_3 n)
+# Space: O(log_3 n)
+def solution(n):
+    
+    digits = ['4', '1', '2']
+    res = ''
+    
+    while n > 0:
+        remainder = n % 3
+        n = (n - 1) // 3
+        
+        res = digits[remainder] + res
+    
+    return res

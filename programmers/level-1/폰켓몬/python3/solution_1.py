1c243da424f63bcf547476f29b10ee90ca8fc04b
Jeongho Ahn <totoroahn@gmail.com>
2026-05-17 20:58:17 +0900

    Update solution_1.py

--- programmers/level-1/폰켓몬/python3/solution_1.py
+++ programmers/level-1/폰켓몬/python3/solution_1.py
@@ -1,10 +1,9 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/1845
-# strategy: hashset
-# time: O(N)
-# space: O(N)
+# Strategy: Hashset
+# Time: O(n)
+# Space: O(n)
 def solution(nums):
-    
     max_pick = len(nums) // 2
-    unique_types = len(set(nums))  # time: O(N)
+    unique_types = len(set(nums))  # O(n)
     
     return min(max_pick, unique_types)

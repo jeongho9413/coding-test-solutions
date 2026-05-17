c0cff5dc2783d604fe41a738d2ad0153e3cf5c33
Jeongho Ahn <totoroahn@gmail.com>
2026-05-17 20:29:49 +0900

    Update solution_1.py

--- programmers/level-1/K번째수/python3/solution_1.py
+++ programmers/level-1/K번째수/python3/solution_1.py
@@ -1,14 +1,14 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42748
-# strategy: sorting
-# time: O(M \times N log N)
-# space: O(N)
+# Strategy: Sorting
+# Time: O(m \times n log n)
+# Space: O(n)
 def solution(array, commands):
     res = list()
     
-    for command in commands:
+    for command in commands:  # O(m)
         i, j, k = command
         temp = array[i - 1 : j]
-        temp.sort()
+        temp.sort()  # O(n log n)
         res.append(temp[k - 1])
         
     return res
